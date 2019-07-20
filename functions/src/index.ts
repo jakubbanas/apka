a21bd193e080602363ff9185b32670ae96f96d5c
Jakub Banas <kuba792@gmail.com>
2019-07-20 11:49:11 +0200

    add google apis

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,24 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { google } from "googleapis";
 
 admin.initializeApp();
-const db = admin.firestore();
-const docRef = db.collection("questions_test");
 
-export const helloWorld = functions.https.onRequest(
-  async (request, response) => {
-    const doc = await docRef.add({ hello: "world" });
-    response.send(doc.id);
-  }
-);
+export const helloWorld = functions.https.onRequest((request, response) => {
+  const sheets = google.sheets({ version: "v4" });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: "1inQBXFZakZgGYF__bdsfE6_HBrVaLL3__E1e4fz7wgs",
+      range: "Class Data!A2:E"
+    },
+    (err: Error | null, res: any) => {
+      if (err) return response.send("The API returned an error: " + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        return response.send(JSON.stringify(rows));
+      } else {
+        return response.send("No data found.");
+      }
+    }
+  );
+});
